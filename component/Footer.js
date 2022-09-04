2a84e27b5e40c2a10e12fc66d88b45bcb39706b6
Nash430 <sihuai430@gmail.com>
2022-09-05 05:46:09 +0800

    IMG adjust

--- component/Footer.js
+++ component/Footer.js
@@ -1,10 +1,12 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className="FooterStyle">
       <div className="iconImg">
-        <img src="./images/icon_YT.png" />
+        <img src="./images/icon_YT.png"  />
         <img src="./images/icon_podcast.png" />
         <img src="./images/icon_IG.png" />
         <img src="./images/icon_facebook.png" />
